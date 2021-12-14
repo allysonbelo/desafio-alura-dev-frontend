b50714c448a14042b6b71cad0fd74af464d27436
Allyson <46270902+allysonbelo@users.noreply.github.com>
2021-12-14 10:40:06 -0300

    :sparkles: Focando link ao clicar no menu lateral

---
+++ js/ativar-link.js
@@ -0,0 +1,16 @@
+const divEditorCodigo = document.querySelector("#editor-codigo")
+const divComunidade = document.querySelector("#comunidade")
+
+const linkCodigo = document.querySelector("#link-codigo")
+const linkComunidade = document.querySelector("#link-comunidade")
+
+if(linkCodigo == document.URL){
+    divEditorCodigo.classList.add("selected")
+    divComunidade.classList.remove("selected")
+}
+
+else if(linkComunidade  == document.URL){
+    divEditorCodigo.classList.remove("selected")
+    divComunidade.classList.add("selected")
+}
+5
